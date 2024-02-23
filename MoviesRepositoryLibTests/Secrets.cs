5c31c3bce0a54905ef52353512fd31328ac575e3
Anders Børjesson <anbo@zealand.dk>
2024-02-23 08:55:34 +0100

    Update Secrets.cs

--- MoviesRepositoryLibTests/Secrets.cs
+++ MoviesRepositoryLibTests/Secrets.cs
@@ -5,6 +5,6 @@ namespace MoviesRepositoryLib.Tests
     public class Secrets
     {
         public static readonly string ConnectionStringSimply =
-            "Data Source=mssql7.unoeuro.com;Initial Catalog=anbo_zealand_dk_db_anbo_base;Persist Security Info=True;User ID=anbo_zealand_dk;Password=FEkfzc2ndpg6DraxB4H3;Trust Server Certificate=True";
+            "Data Source=mssql7.unoeuro.com;Initial Catalog=anbo_zealand_dk_db_anbo_base;Persist Security Info=True;User ID=anbo_zealand_dk;Password=****;Trust Server Certificate=True";
     }
 }
